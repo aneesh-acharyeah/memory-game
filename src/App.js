fd091cd8c825fe664e4780119d9c3a1712d66158
Aneesh <aneeshg2001@gmail.com>
2025-07-03 22:35:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-=======
 import React, { useState, useEffect } from "react";
 import GameBoard from "./components/GameBoard";
 import "./App.css";
@@ -107,6 +82,5 @@ const App = () => {
     </div>
   );
 };
->>>>>>> master
 
 export default App;
